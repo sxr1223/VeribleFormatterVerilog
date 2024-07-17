7ed2c1f54d97ac9f79c17f4e5f68026f0f7d5eb7
sxr <1173368667@qq.com>
2024-07-17 16:36:18 +0800

    Add feat: auto exclude `include for successfully format.

--- VeribleFormatterVerilog.py
+++ VeribleFormatterVerilog.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 import subprocess
 import threading
+import time
 
 import sys
 import os #temp file removal
@@ -14,13 +15,12 @@ SETTINGS_FILE = "VeribleFormatterVerilog.sublime-settings"
 class FormatWithVeribleCommand(sublime_plugin.TextCommand):
  
     def run(self, edit):
-        threading.Thread(target=self.format_with_verible).start()
-
-
-    def format_with_verible(self):
         view = self.view
         # 获取当前文件路径
         file_path = view.file_name()
+        view.run_command("save")
+
+        self.add_comment(view, edit)
 
         settings = sublime.load_settings(SETTINGS_FILE)
         # print(sys.version)
@@ -34,7 +34,7 @@ class FormatWithVeribleCommand(sublime_plugin.TextCommand):
                     flags_file_path = os.path.join(sublime.packages_path(), 'VeribleFormatterVerilog/flags.txt')
                 command = ["verible-verilog-format ", "--flagfile", flags_file_path]
             else:
-                command = ["verible-verilog-format", "--inplace"]
+                command = ["verible-verilog-format"]
                 command.append("--indentation_spaces="+str(settings.get("tab_size",2)))
                 command.append("--column_limit="+str(settings.get("column_limit ",100)))
                 command.append("--line_break_penalty="+str(settings.get("line_break_penalty",2)))
@@ -70,12 +70,44 @@ class FormatWithVeribleCommand(sublime_plugin.TextCommand):
             try:
                 # 执行命令
                 cmd_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                cmd_output = cmd_process.communicate()[1].replace(file_path+":","")
-                if(cmd_output==""):
+                cmd_output = cmd_process.communicate()
+                # print(cmd_output)
+                if(cmd_output[1]==""):
+                    self.remove_comment(view, edit, cmd_output[0])
                     sublime.message_dialog("Verilog 代码格式化成功")
                 else:
-                    sublime.message_dialog("Verilog 代码格式化失败，存在语法错误：\n{}".format(cmd_output))
+                    error_message = cmd_output[1].replace(file_path+":","")[1:]
+                    sublime.message_dialog("Verilog 代码格式化失败，存在语法错误：\n{}".format(error_message))
             except subprocess.CalledProcessError as e:
                 sublime.error_message("Verilog 代码格式化失败：{}".format(e))
         else:
             sublime.error_message("无法获取当前文件路径")
+
+    def add_comment(self, view, edit):
+        region_all = sublime.Region(0, view.size())
+        file_text = view.substr(region_all)
+        lines=file_text.split('\n')
+
+        processed_text = ""
+        for i in lines:
+            if(i.find(r'`include')!=-1):
+                print(i)
+                i="//"+i.lstrip()
+            processed_text+=i+'\n'
+        processed_text=processed_text[:-1]
+        view.replace(edit,region_all,processed_text)
+        view.run_command("save")
+
+    def remove_comment(self, view, edit, str_formatted):
+        region_all = sublime.Region(0, view.size())
+        file_text = str_formatted
+        lines=file_text.split('\n')
+
+        processed_text = ""
+        for i in lines:
+            if(i.find(r"`include")!=-1 and i.find("//")!=-1):
+                print(i)
+                i=i.replace("//","")
+            processed_text+=i+"\n"
+        processed_text=processed_text[:-1]
+        view.replace(edit,region_all,processed_text)
