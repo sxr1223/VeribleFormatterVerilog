24be9f159092df6a073e88569af5aa80384d992d
sxr <1173368667@qq.com>
2024-08-09 09:56:27 +0800

    Bug fix: fix file encoding detect method, avoid get `None` But I found using `subprocess.Popen` will call windows terminal and not exit. Add `subprocess.STARTF_USESHOWWINDOW` flag lead sublime to stuck.  I don't know how to solve that, so I change `subprocess.Popen` to `subprocess.run`.

--- VeribleFormatterVerilog.py
+++ VeribleFormatterVerilog.py
@@ -23,59 +23,63 @@ class FormatWithVeribleCommand(sublime_plugin.TextCommand):
         self.add_comment(view, edit)
 
         settings = sublime.load_settings(SETTINGS_FILE)
-        # print(sys.version)
-        # print(dir(settings))
+
         if file_path:
             # 调用 Verible 格式化 Verilog 代码
-            flags_file_path = settings.get("flags_file_path","")
-            print(flags_file_path)
+            flags_file_path = settings["flags_file_path"]
             if(flags_file_path!=""):
                 if(flags_file_path == "example"):
                     flags_file_path = os.path.join(sublime.packages_path(), 'VeribleFormatterVerilog/flags.txt')
                 command = ["verible-verilog-format ", "--flagfile", flags_file_path]
             else:
                 command = ["verible-verilog-format"]
-                command.append("--indentation_spaces="+str(settings.get("tab_size",2)))
-                command.append("--column_limit="+str(settings.get("column_limit ",100)))
-                command.append("--line_break_penalty="+str(settings.get("line_break_penalty",2)))
-                command.append("--over_column_limit_penalty="+str(settings.get("over_column_limit_penalty ",100)))
-                command.append("--wrap_spaces="+str(settings.get("line_break_penalty",4)))
+                command.append("--indentation_spaces="+str(settings["tab_size"]))
+                command.append("--column_limit="+str(settings["column_limit"]))
+                command.append("--line_break_penalty="+str(settings["line_break_penalty"]))
+                command.append("--over_column_limit_penalty="+str(settings["over_column_limit_penalty"]))
+                command.append("--wrap_spaces="+str(settings["line_break_penalty"]))
 
-                command.append("--assignment_statement_alignment="+str(settings.get("assignment_statement_alignment","flush-left")))
-                command.append("--case_items_alignment="+str(settings.get("case_items_alignment","flush-left")))
-                command.append("--class_member_variable_alignment="+str(settings.get("class_member_variable_alignment","flush-left")))
-                command.append("--compact_indexing_and_selections="+str(settings.get("compact_indexing_and_selections",True)).lower())
-                command.append("--distribution_items_alignment="+str(settings.get("distribution_items_alignment","flush-left")))
-                command.append("--enum_assignment_statement_alignment="+str(settings.get("enum_assignment_statement_alignment","flush-left")))
-                command.append("--expand_coverpoints="+str(settings.get("expand_coverpoints",False)).lower())
-                command.append("--formal_parameters_alignment="+str(settings.get("formal_parameters_alignment","flush-left")))
-                command.append("--formal_parameters_indentation="+str(settings.get("formal_parameters_indentation","wrap")))
-                command.append("--module_net_variable_alignment="+str(settings.get("module_net_variable_alignment","flush-left")))
-                command.append("--named_parameter_alignment="+str(settings.get("named_parameter_alignment","flush-left")))
-                command.append("--named_parameter_indentation="+str(settings.get("named_parameter_indentation","wrap")))
-                command.append("--named_port_alignment="+str(settings.get("named_port_alignment","flush-left")))
-                command.append("--named_port_indentation="+str(settings.get("named_port_indentation","wrap")))
-                command.append("--port_declarations_alignment="+str(settings.get("port_declarations_alignment","flush-left")))
-                command.append("--port_declarations_indentation="+str(settings.get("port_declarations_indentation","wrap")))
-                command.append("--port_declarations_right_align_packed_dimensions="+str(settings.get("port_declarations_right_align_packed_dimensions",False)).lower())
-                command.append("--port_declarations_right_align_unpacked_dimensions="+str(settings.get("port_declarations_right_align_unpacked_dimensions",False)).lower())
-                command.append("--struct_union_members_alignment="+str(settings.get("struct_union_members_alignment","flush-left")))
-                command.append("--try_wrap_long_lines="+str(settings.get("try_wrap_long_lines",False)).lower())     
-                command.append("--wrap_end_else_clauses="+str(settings.get("wrap_end_else_clauses",True)).lower())
-            
-
+                command.append("--assignment_statement_alignment="+str(settings["assignment_statement_alignment"]))
+                command.append("--case_items_alignment="+str(settings["case_items_alignment"]))
+                command.append("--class_member_variable_alignment="+str(settings["class_member_variable_alignment"]))
+                command.append("--compact_indexing_and_selections="+str(settings["compact_indexing_and_selections"]).lower())
+                command.append("--distribution_items_alignment="+str(settings["distribution_items_alignment"]))
+                command.append("--enum_assignment_statement_alignment="+str(settings["enum_assignment_statement_alignment"]))
+                command.append("--expand_coverpoints="+str(settings["expand_coverpoints"]).lower())
+                command.append("--formal_parameters_alignment="+str(settings["formal_parameters_alignment"]))
+                command.append("--formal_parameters_indentation="+str(settings["formal_parameters_indentation"]))
+                command.append("--module_net_variable_alignment="+str(settings["module_net_variable_alignment"]))
+                command.append("--named_parameter_alignment="+str(settings["named_parameter_alignment"]))
+                command.append("--named_parameter_indentation="+str(settings["named_parameter_indentation"]))
+                command.append("--named_port_alignment="+str(settings["named_port_alignment"]))
+                command.append("--named_port_indentation="+str(settings["named_port_indentation"]))
+                command.append("--port_declarations_alignment="+str(settings["port_declarations_alignment"]))
+                command.append("--port_declarations_indentation="+str(settings["port_declarations_indentation"]))
+                command.append("--port_declarations_right_align_packed_dimensions="+str(settings["port_declarations_right_align_packed_dimensions"]).lower())
+                command.append("--port_declarations_right_align_unpacked_dimensions="+str(settings["port_declarations_right_align_unpacked_dimensions"]).lower())
+                command.append("--struct_union_members_alignment="+str(settings["struct_union_members_alignment"]))
+                command.append("--try_wrap_long_lines="+str(settings["try_wrap_long_lines"]).lower())     
+                command.append("--wrap_end_else_clauses="+str(settings["wrap_end_else_clauses"]).lower())
             command.append(file_path)
-
-            print(command)
             try:
-                # 执行命令
-                cmd_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                encoding = view.settings().get('force_encoding')
+                if not encoding:
+                    encoding = view.settings().get('origin_encoding')
 
-                encode = view.settings().get('force_encoding')
-                cmd_output = cmd_process.stdout.read().decode(encode).replace('\x0d','')
-                cmd_err_output = cmd_process.stderr.read().decode(encode)
-                # print('aaaaaaaaaaaaaaaaaaaaaaa')
-                # print(cmd_output)
+                # 执行命令
+                # st=subprocess.STARTUPINFO()
+                # st.dwFlags=subprocess.STARTF_USESHOWWINDOW
+                
+                # startupinfo = subprocess.STARTUPINFO()
+                # startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+                command_res = ' '.join(command)
+                print(command_res)
+                # cmd_process = subprocess.Popen(command, startupinfo=startupinfo, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                cmd_process = subprocess.run(command_res, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                cmd_output = cmd_process.stdout.decode(encoding).replace('\x0d','')
+                cmd_err_output = cmd_process.stderr.decode(encoding)
                 if(cmd_err_output==''):
                     self.remove_comment(view, edit, cmd_output)
                     sublime.message_dialog("Verilog 代码格式化成功")
@@ -95,7 +99,6 @@ class FormatWithVeribleCommand(sublime_plugin.TextCommand):
         processed_text = ""
         for i in lines:
             if(i.find(r'`include')!=-1):
-                print(i)
                 i="//"+i.lstrip()
             processed_text+=i+'\n'
         processed_text=processed_text[:-1]
@@ -110,7 +113,6 @@ class FormatWithVeribleCommand(sublime_plugin.TextCommand):
         processed_text = ""
         for i in lines:
             if(i.find(r"`include")!=-1 and i.find("//")!=-1):
-                print(i)
                 i=i.replace("//","")
             processed_text+=i+"\n"
         processed_text=processed_text[:-1]
